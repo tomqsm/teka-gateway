226be6895283995f204d1442507d37744495d345
Tomasz Kusmierczyk <tomasz.kusmierczyk@letsweb.biz>
2015-07-25 00:56:50 +0200

    add logger

--- rest-one/src/main/java/biz/lesweb/rest/one/RestOneApp.java
+++ rest-one/src/main/java/biz/lesweb/rest/one/RestOneApp.java
@@ -3,6 +3,8 @@ package biz.lesweb.rest.one;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -10,7 +12,11 @@ import org.springframework.context.ApplicationContext;
  */
 @SpringBootApplication
 public class RestOneApp {
+
+    static final Logger LOG = LoggerFactory.getLogger(RestOneApp.class);
+
     public static void main(String[] args) {
+        LOG.info("Initialising ...");
         ApplicationContext ctx = SpringApplication.run(RestOneApp.class, args);
     }
 }
