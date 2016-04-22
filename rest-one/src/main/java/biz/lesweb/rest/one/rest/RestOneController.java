a1db0ebccc46bbc96c90767a0c276526e1b60f32
Tomasz <tomasz.kusmierczyk@letsweb.biz>
2016-04-22 13:57:25 +0200

    add testing json response in mocked controller test

--- rest-one/src/main/java/biz/lesweb/rest/one/rest/RestOneController.java
+++ rest-one/src/main/java/biz/lesweb/rest/one/rest/RestOneController.java
@@ -1,10 +1,13 @@
 package biz.lesweb.rest.one.rest;
 
 import biz.lesweb.rest.one.rest.service.api.ServiceRestOne;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 /**
  *
@@ -12,13 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class RestOneController {
-    
+
     @Autowired
     private ServiceRestOne serviceRestOne;
-    
+
     @RequestMapping("/")
-    public String index() { 
+    public String index() {
         serviceRestOne.doOne();
         return "Greetings from Rest 1!";
+    }
+
+    @RequestMapping(value="/getjson", produces = MediaType.APPLICATION_JSON_VALUE)
+    public JsonNode getJson() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        String message = "{\"message\":\"hello world\"}";
+        return mapper.readTree(message);
     }
 }
--- rest-one/src/test/java/biz/lesweb/rest/one/rest/RestOneControllerTest.java
+++ rest-one/src/test/java/biz/lesweb/rest/one/rest/RestOneControllerTest.java
@@ -1,5 +1,8 @@
 package biz.lesweb.rest.one.rest;
+
 import biz.lesweb.rest.one.rest.service.api.ServiceRestOne;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import static org.hamcrest.core.StringContains.containsString;
 import org.junit.After;
 import org.junit.Before;
@@ -19,6 +22,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -34,12 +38,12 @@ import org.springframework.util.StringUtils;
 @SpringApplicationConfiguration(classes = MockServletContext.class) //sets up an empty mvc context
 @WebAppConfiguration
 public class RestOneControllerTest {
-
+    
     @InjectMocks
     private RestOneController restOneController;
-
+    
     private MockMvc mockMvc;
-
+    
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
@@ -48,18 +52,17 @@ public class RestOneControllerTest {
                 .setMessageConverters(new MappingJackson2HttpMessageConverter())
                 .build();
     }
-
+    
     @After
     public void tearDown() {
     }
-
-
+    
     @Test
     public void controllerCleanPathShowcase() throws Exception {
         String path = "admin/./error";
         Assertions.assertThat(StringUtils.cleanPath(path)).isEqualTo("admin/error");
     }
-
+    
     @Test
     public void controllerWithMockedContextNeedsManualInjectionOfAutowiredField() throws Exception {
         final ServiceRestOne serviceRestOne = Mockito.mock(ServiceRestOne.class);
@@ -69,6 +72,7 @@ public class RestOneControllerTest {
             System.out.println("Hello world");
             return null;
         }).when(serviceRestOne).doOne();
+        
         ReflectionTestUtils.setField(restOneController, "serviceRestOne", serviceRestOne);
         
         final MockHttpServletRequestBuilder get = get("/")
@@ -76,7 +80,28 @@ public class RestOneControllerTest {
         this.mockMvc.perform(get)
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Greetings from Rest 1!")));
-
+        
     }
-
+    
+    @Test
+    public void controllerSendsJson() throws Exception {
+        final ServiceRestOne serviceRestOne = Mockito.mock(ServiceRestOne.class);
+        
+        ReflectionTestUtils.setField(restOneController, "serviceRestOne", serviceRestOne);
+        
+        final MockHttpServletRequestBuilder get = get("/getjson")
+                .accept(MediaType.APPLICATION_JSON);
+        final MvcResult restResult = this.mockMvc.perform(get)
+                .andExpect(status().isOk())
+                .andReturn();
+        
+        final String contentAsString = restResult.getResponse().getContentAsString();
+        
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readValue(contentAsString, JsonNode.class);
+        
+        JsonNode data = root.get("message");
+        Assertions.assertThat(data.asText()).isEqualTo("hello world");
+    }
+    
 }
