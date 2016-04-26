3d62590ca2034d4be6759e03b2bddd1833504ae1
Tomasz <tomasz.kusmierczyk@letsweb.biz>
2016-04-26 15:19:14 +0200

    with property object via yaml file

--- camel-integration/src/main/java/biz/letsweb/camel/integration/IntegrationService.java
+++ camel-integration/src/main/java/biz/letsweb/camel/integration/IntegrationService.java
@@ -1,7 +1,10 @@
 package biz.letsweb.camel.integration;
 
-import java.net.ConnectException;
+import java.util.Map;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,8 +14,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class IntegrationService extends RouteBuilder {
 
+    @Autowired
+    LoadBalancerProps balancerProps;
+
     @Override
     public void configure() throws Exception {
+        final Map<String, String> props = balancerProps.getLoadbalancer();
+        System.out.println(String.format("XX yeah %s", props));
 //        from("file:data/inbox?noop=true")
 //                .choice().when(header("CamelFileName").endsWith(".xml"))
 //                .process(new Processor() {
@@ -26,15 +34,23 @@ public class IntegrationService extends RouteBuilder {
 //                    }
 //                }).convertBodyTo(String.class)
 //                .to("file:data/outbox");
-        from("jetty://http://localhost:8083?matchOnUriPrefix=true&throwExceptionOnFailure=false")
+        from(String.format("%s:%s?%s&%s", 
+                props.get("component"),
+                props.get("host"),
+                props.get("matchOnUriPrefix"),
+                props.get("throwExceptionOnFailure")))
+                .process((Exchange exchange) -> {
+                    Message message = exchange.getIn();
+                    System.out.println(message.getHeaders());
+                })
                 .loadBalance()
                 //                .random()
-                .failover(2, true, true, ConnectException.class)
-                //                .roundRobin()
+                //                                .failover(2, true, true, ConnectException.class)
+                .roundRobin()
                 .to(
                         //                        "http://localhost:8080?throwExceptionOnFailure=true&bridgeEndpoint=true",
-                        "http://localhost:8070?throwExceptionOnFailure=true&bridgeEndpoint=true",
-                        "http://localhost:8071?throwExceptionOnFailure=true&bridgeEndpoint=true"
+                        "http://localhost:8070/rest-one?throwExceptionOnFailure=true&bridgeEndpoint=true"
+                //                        "http://localhost:8071?throwExceptionOnFailure=true&bridgeEndpoint=true"
                 );
     }
 
